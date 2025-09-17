ad21c28d4c0dd47fbe96f6a399451169bf661c05
PiyushCodess <piyushpatrikar459@gmail.com>
2025-09-17 19:16:28 +0530

    c7

---
+++ maximum_subarray.py
@@ -0,0 +1,7 @@
+def maxSubArray(self, nums: list[int]) -> int:
+        max_sum = nums[0]
+        curr_sum = nums[0]
+        for num in nums[1:]:
+            curr_sum = max(num, curr_sum + num)
+            max_sum = max(max_sum, curr_sum)
+        return max_sum
